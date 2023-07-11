9423d49273cc271649a622ff328edb728e104f4f
deliverytawika <139254910+deliverytawika@users.noreply.github.com>
2023-07-11 17:21:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,48 @@
-function sendData() {
-	var name = $('#name').val();
-	var phoneNumber = $('#phonenumber').val();
-	var order = $('#order').val();
-	var location = $('#location').val();
+function sendOrder() {
+    var name = $("#name").val();
+    var phone = $("#phone").val();
+    var order = $("#order").val();
+    var location = $("#location").val();
 
-	if (name != '' && phoneNumber != '' && order != '' && location != '') {
-		$.post('/send-message', {name: name, phoneNumber: phoneNumber, order: order, location: location}, function(response) {
-			alert(response.message);
-		});
-	} else {
-		alert('Please fill in all the fields.');
-	}
+    // Generate order number
+    var orderNumber = generateOrderNumber();
+
+    // Send order data to Telegram bot API endpoint via AJAX POST request
+    $.ajax({
+        url: "https://api.telegram.org/bot6351918844:AAGcj7hXJie0jFm_-7ZElanCretnIY7Kufg/sendMessage",
+        type: "POST",
+        data: {
+            chat_id: "<YOUR_CHAT_ID>",
+            text: "New Order Details:\n"
+                + "Order Number: " + orderNumber + "\n"
+                + "Name: " + name + "\n"
+                + "Phone Number: " + phone + "\n"
+                + "Order: " + order + "\n"
+                + "Location: " + location
+        },
+        success: function(response) {
+            alert("Order submitted successfully! Your order number is: " + orderNumber);
+            resetForm();
+        },
+        error: function(xhr, status, error) {
+            alert("There was an error submitting the order: " + error);
+        }
+    });
+}
+
+function generateOrderNumber() {
+    var date = new Date();
+    var orderNumber = "ORD-" + date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + "-" + getRandomInteger(1000, 9999);
+    return orderNumber;
+}
+
+function getRandomInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function resetForm() {
+    $("#name").val("");
+    $("#phone").val("");
+    $("#order").val("");
+    $("#location").val("");
 }
